d69cd4e3a341bf99cf8784e71b48e9591175dc9a
Preema Rahman <preemanawal@gmail.com>
2015-09-20 19:53:20 +1000

    First commit

---
+++ GameView.c
@@ -0,0 +1,99 @@
+// GameView.c ... GameView ADT implementation
+
+#include <stdlib.h>
+#include <assert.h>
+#include "Globals.h"
+#include "Game.h"
+#include "GameView.h"
+// #include "Map.h" ... if you decide to use the Map ADT
+     
+struct gameView {
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    Round gameRound;
+    int score;
+    PlayerID currentTurn;
+    Players players[];
+    char *pastPlays;
+};
+
+typedef struct playerStat {
+    int health;
+    LocationID location;
+    LocationID pastLocations[];
+} Players;
+     
+
+// Creates a new GameView to summarise the current state of the game
+GameView newGameView(char *pastPlays, PlayerMessage messages[])
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    GameView gameView = malloc(sizeof(struct gameView));
+    gameView->score = GAME_START_SCORE;
+
+    return gameView;
+}
+     
+     
+// Frees all memory previously allocated for the GameView toBeDeleted
+void disposeGameView(GameView toBeDeleted)
+{
+    //COMPLETE THIS IMPLEMENTATION
+    free( toBeDeleted );
+}
+
+
+//// Functions to return simple information about the current state of the game
+
+// Get the current round
+Round getRound(GameView currentView)
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    return currentView->gameRound;
+}
+
+// Get the id of current player - ie whose turn is it?
+PlayerID getCurrentPlayer(GameView currentView)
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    return currentView->currentTurn;;
+}
+
+// Get the current score
+int getScore(GameView currentView)
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    return currentView->score;
+}
+
+// Get the current health points for a given player
+int getHealth(GameView currentView, PlayerID player)
+{
+    return currentView->players[player]->health;;
+}
+
+// Get the current location id of a given player
+LocationID getLocation(GameView currentView, PlayerID player)
+{
+    return currentView->players[player]->location;;
+}
+
+//// Functions that return information about the history of the game
+
+// Fills the trail array with the location ids of the last 6 turns
+void getHistory(GameView currentView, PlayerID player,
+                            LocationID trail[TRAIL_SIZE])
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+}
+
+//// Functions that query the map to find information about connectivity
+
+// Returns an array of LocationIDs for all directly connected locations
+
+LocationID *connectedLocations(GameView currentView, int *numLocations,
+                               LocationID from, PlayerID player, Round round,
+                               int road, int rail, int sea)
+{
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    return NULL;
+}
